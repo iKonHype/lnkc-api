1606079402312eca0b379ccad3212d1527a4c920
Kaveen Hyacinth <alwiskaveen@gmail.com>
2023-05-05 20:34:43 +0530

    modify static redirection

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,20 +2,16 @@ import { Controller, Get, Param, Redirect } from '@nestjs/common';
 import { LinkDto } from 'src/link/dtos/link.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AppService } from './app.service';
-import { ConfigService } from '@nestjs/config';
 
 @Controller()
 @Serialize(LinkDto)
 export class AppController {
-  constructor(
-    private appService: AppService,
-    private configService: ConfigService,
-  ) {}
+  constructor(private appService: AppService) {}
 
   @Get()
-  @Redirect('', 302)
+  @Redirect('https://site.lnkc.xyz', 302)
   async redirectToClient() {
-    return this.configService.get<string>('CLIENT_ORIGIN');
+    return '';
   }
 
   @Get(':shortCode')
