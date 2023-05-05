69daf59c80eeb41c6e7bb8ac652241445550f9f4
kaveenhyacinth <alwiskaveen@gmail.com>
2023-05-05 20:11:55 +0530

    add 301 redirection to client site on root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,25 @@ import { Controller, Get, Param, Redirect } from '@nestjs/common';
 import { LinkDto } from 'src/link/dtos/link.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 @Serialize(LinkDto)
 export class AppController {
-  constructor(private appService: AppService) {}
+  constructor(
+    private appService: AppService,
+    private configService: ConfigService,
+  ) {}
+
+  @Get()
+  @Redirect('', 301)
+  async redirectToClient() {
+    console.log(
+      'redirecting to ',
+      this.configService.get<string>('CLIENT_ORIGIN'),
+    );
+    return this.configService.get<string>('CLIENT_ORIGIN');
+  }
 
   @Get(':shortCode')
   @Redirect('', 302)
